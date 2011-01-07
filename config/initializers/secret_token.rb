78b27fcd2356cc326f184eedf70cdf4c3f80d393
Juanjo <juanjo.martinez@sci-spain.com>
2011-01-07 11:14:12 +0000

    Prueba de jQuery Mobile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '467d0747ba7ec52b10f9409cf7a81f87cb79f1869d7143c3fca4c0f15c56982d81535f424fa57d1df2fb27076e753c203e18cca808be58d77bce228b921467f2'
